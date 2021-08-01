4e89ee880a083300028ee99724835db8a749c88b
Yuvaraj P <71285826+Yuvaraj-P@users.noreply.github.com>
2021-08-01 23:06:52 +0530

    Update temporal.py

--- temporal.py
+++ temporal.py
@@ -4,10 +4,13 @@ Spyder Editor
 
 This is a temporary script file.
 """
+#! python3
+
 import cv2
 import numpy as np
-from ffpyplayer.player import MediaPlayer as mply 
+#from ffpyplayer.player import MediaPlayer as mply 
 from skimage import measure
+import csv
 
 #Defining the mean squared error
 def mse(imga,imgb):
@@ -18,44 +21,39 @@ def mse(imga,imgb):
     return err
 
 def main():
-
-    #Read the frame from the video
-    cap = cv2.VideoCapture("C:\\Users\\yuvar\\Documents\\Bandicam\\screen2.mp4")
+    
+    # module 1: Read the frame from the video and init var
+    cap = cv2.VideoCapture("C:\\Users\\yuvar\\Documents\\temporal_vc\\input_video\\srcrec.mp4")
     check,current_frame = cap.read()
+    
     #Initializing the variables
     frame_list = []
     counter = 0
     frame_no=[]
     check = True
     choice = 1 #default choice
-    
     print("Choose the method for key frame Extraction :")
     print(" 1. Subtraction (default) ")
     print(" 2. Mean Squared Error ")
     print(" 3. Structaral similarity index ")
     choice = int(input("Enter the choice :"))
     
+    shape = current_frame.shape
     frame_list.append(current_frame)
-    print("Shape of video frame :" + str(current_frame.shape))
+    frame_no.append(counter)
+    print("Shape of video frame :" + str(shape))
     n=cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    
+    #module 2 mapper
     for i in range(int(n)-1):
-        cv2.imwrite("frame%d.jpg" %counter ,current_frame)
-        frame_no.append(counter)
+        #cv2.imwrite("frame%d.jpg" %counter ,current_frame)
         check,current_frame = cap.read()
-        #subtraction
-        if(choice == 1):
-            diff =cv2.subtract(frame_list[counter],current_frame)
-            b,g,r = cv2.split(diff)
-            if cv2.countNonZero(b)==0 and cv2.countNonZero(g)==0 and cv2.countNonZero(r)==0:
-                continue
-            else:
-                frame_list.append(current_frame)
-                counter+=1
         #MSE
-        elif(choice == 2):
+        if(choice == 2):
             diff = mse(frame_list[-1],current_frame)
         
-            if(diff <= 100):
+            if(diff <= 10):
+                frame_no.append(counter)
                 continue
             else:
                 frame_list.append(current_frame)
@@ -69,32 +67,66 @@ def main():
             sg = measure.compare_ssim(g1,g2)
             sr = measure.compare_ssim(r1,r2)
             
-            if(sb >= 0.9 and sg >= 0.9 and sr >= 0.9):
+            if(sb >= 0.95 and sg >= 0.95 and sr >= 0.95):
+                frame_no.append(counter)
                 continue
             else:
                 frame_list.append(current_frame)
                 counter+=1
-                
+                #subtraction
+        else:
+            diff =cv2.subtract(frame_list[counter],current_frame)
+            b,g,r = cv2.split(diff)
+            if cv2.countNonZero(b)==0 and cv2.countNonZero(g)==0 and cv2.countNonZero(r)==0:
+                frame_no.append(counter)
+                continue
+            else:
+                frame_list.append(current_frame)
+                counter+=1
+        
+        frame_no.append(counter)
+        
+        
+    check=True
+    while(check==True):
+        check,current_frame = cap.read()
+        frame_list.append(current_frame)
+        frame_no.append(counter)
+        
     print("\nInitial number of frame : " + str(cap.get(cv2.CAP_PROP_FRAME_COUNT)+1) )
     print("Total key_frames after extraction :"+ str(counter))
+    print("The Frame No are:")
     print(frame_no)
+    print("length :"+str(len(frame_no)))
+    
     #print(len(frame_no))
     
-    #player = mply("C:\\Users\\yuvar\\Documents\\Bandicam\\screen2.mp4") 
-    
-    #Below code runs based on compressed or extracted key frame
+    with open('C:/Users/yuvar/Documents/temporal_vc/outputs/meta.csv', mode='w') as out_file:
+        out_writer = csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    for i in frame_no:
-        frm = frame_list[i]
-        cv2.imshow("frame",frm)
-        #audio_frame ,val =player.get_frame()
-        if cv2.waitKey(75) & 0xFF==ord('q'):
-            break 
-        if frm.any == None:
-            break
-        
+        out_writer.writerow(frame_no)
+    out_file.close()
+    """
+    if(choice==3):
+        name="_ssmi.mp4"
+    elif(choice==2):
+        name="_mse.mp4"
+    else:
+        name="_diff.mp4"
+    """ 
+    
+    pathout="C:/Users/yuvar/Documents/temporal_vc/outputs/keyframesvid.mp4"
+    height, width, layers = shape
+    size=(width,height)
+    fps=cap.get(cv2.CAP_PROP_FPS)
+    print("Frame rate : "+str(fps))
+    out=cv2.VideoWriter(pathout, cv2.VideoWriter_fourcc(*'mp4v'), fps,size)
+    for i in range(len(frame_list)):
+       out.write(frame_list[i])
+    out.release()
+
     cap.release()
     cv2.destroyAllWindows()
-
+    
 if __name__=="__main__":
     main() 
